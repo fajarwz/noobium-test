4b48ef1bb21aff6d58f17c1673dba3ab8376927b
fajarwz <fajarwindhuzulfikar@gmail.com>
2022-08-30 11:23:05 +0700

    chore: login with google tidy up codes

--- app/Http/Controllers/GoogleAuthController.php
+++ app/Http/Controllers/GoogleAuthController.php
@@ -3,30 +3,18 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
-use Socialite;
-use JWTAuth;
 
 class GoogleAuthController extends Controller
 {
-    public function getUrl()
+    public function signIn($token)
     {
-        return response()->json([
-            'meta' => [
-                'code' => 200,
-                'status' => 'success',
-                'message' => 'Google Sign In Url fetched successfully.',
-            ],
-            'data' => [
-                'url' => Socialite::driver('google')->stateless()->redirect()->getTargetUrl(),
-            ],
-        ]);
-    }
+        $tokenParts = explode('.', $token);
 
-    public function callback()
-    {
-        $user = Socialite::driver('google')->with(['access_type' => 'offline'])->stateless()->user();
+        $tokenPayload = base64_decode($tokenParts[1]);
 
-        if (!$user)
+        $jwtPayload = json_decode($tokenPayload, true);
+
+        if($jwtPayload === null)
         {
             return response()->json([
                 'meta' => [
@@ -38,87 +26,7 @@ class GoogleAuthController extends Controller
             ], 500);
         }
 
-        $finduser = User::where('social_id', $user->id)->first();
-        $token = '';
-
-        if ($finduser)
-        {
-            $token = auth()->login($finduser);
-
-            return response()->json([
-                'meta' => [
-                    'code' => 200,
-                    'status' => 'success',
-                    'message' => 'Signed in successfully.',
-                ],
-                'data' => [
-                    'user' => [
-                        'name' => $finduser->name,
-                        'email' => $finduser->email,
-                        'picture' => $finduser->picture,
-                    ],
-                    'access_token' => [
-                        'token' => $token,
-                        'type' => 'Bearer',
-                        'expires_in' => auth()->factory()->getTTL() * 60,
-                    ],
-                ],
-            ]);
-        }
-        else
-        {
-            $newUser = User::create([
-                'name' => $user->name,
-                'email' => $user->email,
-                'password' => bcrypt('my-google'),
-                'picture' => $user->avatar,
-                'social_id' => $user->id,
-                'social_type' => 'google',
-            ]);
-
-            $token = auth()->login($newUser);
-
-            return response()->json([
-                'meta' => [
-                    'code' => 200,
-                    'status' => 'success',
-                    'message' => 'Signed in successfully.',
-                ],
-                'data' => [
-                    'user' => [
-                        'name' => $newUser->name,
-                        'email' => $newUser->email,
-                        'picture' => $newUser->picture,
-                    ],
-                    'access_token' => [
-                        'token' => $token,
-                        'type' => 'Bearer',
-                        'expires_in' => auth()->factory()->getTTL() * 60,
-                    ],
-                ],
-            ]);
-        }
-
-        return response()->json([
-            'meta' => [
-                'code' => 500,
-                'status' => 'error',
-                'message' => 'Login with Google failed.',
-            ],
-            'data' => [],
-        ], 500);
-    }
-
-    public function withToken($token)
-    {
-        $tokenParts = explode(".", $token);  
-        
-        $tokenPayload = base64_decode($tokenParts[1]);
-        
-        $jwtPayload = json_decode($tokenPayload, true);
-
         $finduser = User::where('social_id', $jwtPayload['sub'])->first();
-        $token = '';
 
         if ($finduser)
         {
@@ -144,47 +52,36 @@ class GoogleAuthController extends Controller
                 ],
             ]);
         }
-        else
-        {
-            $newUser = User::create([
-                'name' => $jwtPayload['name'],
-                'email' => $jwtPayload['email'],
-                'password' => bcrypt('my-google'),
-                'picture' => $jwtPayload['picture'],
-                'social_id' => $jwtPayload['sub'],
-                'social_type' => 'google',
-            ]);
+        
+        $newUser = User::create([
+            'name' => $jwtPayload['name'],
+            'email' => $jwtPayload['email'],
+            'password' => bcrypt('my-google'),
+            'picture' => $jwtPayload['picture'],
+            'social_id' => $jwtPayload['sub'],
+            'social_type' => 'google',
+        ]);
 
-            $token = auth()->login($newUser);
-
-            return response()->json([
-                'meta' => [
-                    'code' => 200,
-                    'status' => 'success',
-                    'message' => 'Signed in successfully.',
-                ],
-                'data' => [
-                    'user' => [
-                        'name' => $newUser->name,
-                        'email' => $newUser->email,
-                        'picture' => $newUser->picture,
-                    ],
-                    'access_token' => [
-                        'token' => $token,
-                        'type' => 'Bearer',
-                        'expires_in' => auth()->factory()->getTTL() * 60,
-                    ],
-                ],
-            ]);
-        }
+        $token = auth()->login($newUser);
 
         return response()->json([
             'meta' => [
-                'code' => 500,
-                'status' => 'error',
-                'message' => 'Login with Google failed.',
+                'code' => 200,
+                'status' => 'success',
+                'message' => 'Signed in successfully.',
             ],
-            'data' => [],
-        ], 500);
+            'data' => [
+                'user' => [
+                    'name' => $newUser->name,
+                    'email' => $newUser->email,
+                    'picture' => $newUser->picture,
+                ],
+                'access_token' => [
+                    'token' => $token,
+                    'type' => 'Bearer',
+                    'expires_in' => auth()->factory()->getTTL() * 60,
+                ],
+            ],
+        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,9 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/sign-up', [AuthController::class, 'signUp'])->name('sign-up');
 Route::post('/sign-in', [AuthController::class, 'signIn'])->name('sign-in');
 
-Route::get('/sign-in/google/get-url', [GoogleAuthController::class, 'getUrl']);
-Route::get('/sign-in/google/callback', [GoogleAuthController::class, 'callback']);
-Route::get('/sign-in/google/with-token/{token}', [GoogleAuthController::class, 'withToken']);
+Route::get('/sign-in/google/{token}', [GoogleAuthController::class, 'signIn']);
 
 Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/categories/{categorySlug}', [CategoryController::class, 'show']);
