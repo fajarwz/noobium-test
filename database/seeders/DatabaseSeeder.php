9805a438227a5041ef21b1803e1f4711b685669d
Fajar Windhu Zulfikar <fajarwindhuzulfikar@gmail.com>
2022-09-11 05:38:14 +0700

    tambah seeder user dan article

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             CategorySeeder::class,
+            UserSeeder::class,
+            ArticleSeeder::class,
         ]);
     }
 }
